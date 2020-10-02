f95b9a5993fb643187993bf4dbcaa4ee4f9a1bbd
micura <micura.adam@gmail.com>
2020-10-03 00:10:48 +0200

    Encode passwords

--- src/main/java/hu/toll/tollWeb/Config/SecurityConf.java
+++ src/main/java/hu/toll/tollWeb/Config/SecurityConf.java
@@ -5,10 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -24,9 +23,9 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
     public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
         auth
             .inMemoryAuthentication()
-            .passwordEncoder(NoOpPasswordEncoder.getInstance())
-            .withUser("admin").password("pass").roles("ADMIN").and()
-            .withUser("user").password("pass").roles("USER").and()
-            .withUser("micura2").password("pass").roles("USER");
+            .passwordEncoder(passwordEncoder())
+            .withUser("admin").password("$2a$10$VZxD0gqknDDuX5ZuyscX/OOBMD1YzXEzA4Le4tCLaAEouskvujBre").roles("ADMIN").and()
+            .withUser("user").password("$2a$10$S1kxoZIuj7u/FN86rZK4Z.AgUL1byzP1rgkmb9/ccSfjH1qtVQrie").roles("USER").and()
+            .withUser("micura2").password("$2a$10$ctJAe1dnVWsQOf4T694yWOKfaXAEt7SWsqSitV.N8ho.2AIk/Oh06").roles("USER");
     }
 }
